996142237cf4418a490721e6003732ccab33f15b
Atahan Ozdemir <atahanozdemir@berkeley.edu>
2025-02-25 23:58:18 -0800

    fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,10 @@ const path = require("path");
 const createNextPluginPreval = require("next-plugin-preval/config");
 const withNextPluginPreval = createNextPluginPreval();
 
+require("dotenv").config({
+  path: path.resolve(__dirname, "../.env"),
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
